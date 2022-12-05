247c12fab90612e15394401fc4eeb54dc512f3ca
Murad Mammadzade <mammadzade03@gmail.com>
2022-12-05 20:23:19 +0400

    I add sass file to project and learn how to compile this file with web compiler extension and generate pure css files for css isolation

--- Blazor_ECommerce/Client/Shared/ProductList.razor.cs
+++ Blazor_ECommerce/Client/Shared/ProductList.razor.cs
@@ -33,7 +33,7 @@ namespace Blazor_ECommerce.Client.Shared
                         Id = 3,
                         Tittle = "Nineteen Eighty-Four",
                         Description = "Nineteen Eighty-Four (also stylised as 1984) is a dystopian social science fiction novel and cautionary tale written by English writer George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime. Thematically, it centres on the consequences of totalitarianism, mass surveillance and repressive regimentation of people and behaviours within society.[2][3] Orwell, a democratic socialist, modelled the totalitarian government in the novel after Stalinist Russia and Nazi Germany.[2][3][4] More broadly, the novel examines the role of truth and facts within politics and the ways in which they are manipulated.",
-                        ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
+                        ImageUrl = "https://static.insales-cdn.com/images/products/1/8/211730440/large_9780141036144.jpg",
                         Price = 6.99m
                     }
 
