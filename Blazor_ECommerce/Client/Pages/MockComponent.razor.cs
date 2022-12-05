5d8d7057a49039b76f42c40e32f1bdf001df9823
Murad Mammadzade <mammadzade03@gmail.com>
2022-12-05 18:45:12 +0400

    I learn how to call component,how to seperate c# code with componentbase and inherits keyword as well as i learn one and two way binding and it's main difference

---
+++ Blazor_ECommerce/Client/Pages/MockComponent.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor_ECommerce.Client.Pages
+{
+    public class MockComponentBase:ComponentBase
+    {
+        public string Name = "Murad";
+    }
+}
