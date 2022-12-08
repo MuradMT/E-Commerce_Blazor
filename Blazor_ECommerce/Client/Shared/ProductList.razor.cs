896261eb42c46422e7fe9526cf8c2051ec6c2ddf
Murad Mammadzade <mammadzade03@gmail.com>
2022-12-08 23:52:26 +0400

    Start getting data to ui  from api with services

--- Blazor_ECommerce/Client/Shared/ProductList.razor.cs
+++ Blazor_ECommerce/Client/Shared/ProductList.razor.cs
@@ -5,21 +5,17 @@ using Blazor_ECommerce.Shared.Models;
 using Microsoft.AspNetCore.Components;
 using System.Net.Http.Json;
 using System.Runtime.CompilerServices;
+using Blazor_ECommerce.Client.Services.ProductService;
 using static System.Net.WebRequestMethods;
 
 namespace Blazor_ECommerce.Client.Shared
 {
     public partial class ProductList
     {
-        //Mock Data
-        [Parameter]
-        public string Name { get; set; }
-        private List<Product> products = new List<Product>();
         protected override async Task OnInitializedAsync()
         {
-            var result = await http.GetFromJsonAsync<List<Product>>("api/Product/Get");
-            if(result != null) products = result;
-            
+            await _prod.GetProducts();
+
         }
     }
 }
