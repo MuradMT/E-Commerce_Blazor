ecc2180c03d983049de2d0704ea7fc93ee9c7016
Murad Mammadzade <mammadzade03@gmail.com>
2022-12-17 08:55:47 +0400

    add ui service to get data

--- Blazor_ECommerce/Client/Services/ProductService/IProductService.cs
+++ Blazor_ECommerce/Client/Services/ProductService/IProductService.cs
@@ -1,11 +1,14 @@
 ﻿
 
+using Blazor_ECommerce.Shared;
+
 namespace Blazor_ECommerce.Client.Services.ProductService
 {
     public interface IProductService
     {
         List<Product> products { get; set; }
         Task GetProducts();
-
+        
+        Task<ServiceResponse<Product>> GetProduct(int id);
     }
 }
--- Blazor_ECommerce/Client/Services/ProductService/ProductService.cs
+++ Blazor_ECommerce/Client/Services/ProductService/ProductService.cs
@@ -12,10 +12,16 @@ namespace Blazor_ECommerce.Client.Services.ProductService
         }
         public List<Product> products { get ; set ; }= new List<Product>();
 
+        public async Task<ServiceResponse<Product>> GetProduct(int id)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/Product/GetProduct/{id}");
+            return result;
+        }
+
         public async Task GetProducts()
         {
             var result = await
-                _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product/Get");
+                _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product/GetProducts");
             if (result != null&&result.Data!=null)
                 products = result.Data;
         }
